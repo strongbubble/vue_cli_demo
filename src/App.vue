66e8be4b6f79adc9426d1370d251bca01e49f51c
strongbubble <1303929162@qq.com>
2018-01-30 16:41:52 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="tabbar">
+      <router-link to="/home">首页</router-link>
+      <router-link to="/market">闪送超市</router-link>
+      <router-link to="/cart">购物车</router-link>
+      <router-link to="/mine">我的</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+#app .tabbar{
+  position:fixed;
+  left:0;
+  right:0;
+  height:45px;
+  bottom:0;
+  background:white;
+  border-top:1px solid #ccc;
+  border-right:1px solid #f5f5f5;
+  display:flex;
+  align-items:center;
+  justify-content:space-around;
+}
+#app .tabbar{
+  
+}
+</style>
